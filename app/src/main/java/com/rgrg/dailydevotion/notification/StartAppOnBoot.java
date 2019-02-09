156734c1118bc59ce50c03e118c8430951eafbe8
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-02-09 10:35:45 +0800

    Changed startActivity to startService

--- app/src/main/java/com/rgrg/dailydevotion/notification/StartAppOnBoot.java
+++ app/src/main/java/com/rgrg/dailydevotion/notification/StartAppOnBoot.java
@@ -4,15 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.rgrg.dailydevotion.MainActivity;
-
 public class StartAppOnBoot extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
             Intent i = new Intent(context, AppService.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(i);
+            context.startService(i);
         }
     }
 }
